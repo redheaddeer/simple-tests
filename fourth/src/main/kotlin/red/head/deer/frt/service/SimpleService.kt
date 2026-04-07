2c8f7fd563556753de7356917d060300a38bd276
Xenia <44500935+redheaddeer@users.noreply.github.com>
2026-04-07 07:39:50 +0300

    Update SimpleService.kt logger

--- fourth/src/main/kotlin/red/head/deer/frt/service/SimpleService.kt
+++ fourth/src/main/kotlin/red/head/deer/frt/service/SimpleService.kt
@@ -6,12 +6,13 @@ import red.head.deer.frt.config.Config
 class SimpleService(
     val config: Config
 ) {
-    val log = logger("red.head.deer.frt.controller")
+    val log = logger("red.head.deer.frt.service")
 
     private val useDb: Boolean = config.useDB
     private val testSet: String = config.testSet
 
     fun start() {
         log.info("Start with params: test-set = $testSet, use db = $useDb")
+        
     }
 }
