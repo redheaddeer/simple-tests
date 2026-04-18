dff48d770d76ac69c2a7fed83020eb09fac34c8c
Xenia <44500935+redheaddeer@users.noreply.github.com>
2026-04-18 10:20:39 +0300

    Update DbService.kt

--- fourth/src/main/kotlin/red/head/deer/frt/service/DbService.kt
+++ fourth/src/main/kotlin/red/head/deer/frt/service/DbService.kt
@@ -1,5 +1,18 @@
+import red.head.deer.frt.config.Config
 class DbService(
   private val config: Config
 ) {
+val insert = """
+INSERT INTO test_result (step, status, result)
+VALUES (:step, :status, :result)
+""" 
 
+val update = """
+UPDATE test_result SET status=:status, result=:result
+WHERE step=:step
+""" 
+
+val selectStatus = """
+SELECT status FROM test_result WHERE step=:step
+""" 
 }
