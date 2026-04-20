5bb444340cd5f10d87177dbdb92ceeb56a09c934
Xenia <44500935+redheaddeer@users.noreply.github.com>
2026-04-20 06:24:07 +0300

    upd

--- fourth/src/main/kotlin/red/head/deer/frt/service/DbService.kt
+++ fourth/src/main/kotlin/red/head/deer/frt/service/DbService.kt
@@ -1,22 +1,25 @@
+package red.head.deer.frt.service
+
 import red.head.deer.frt.config.Config
+
 class DbService(
-  private val config: Config
+    private val config: Config
 ) {
-val insert = """
+    val insert = """
 INSERT INTO test_result (step, status, result)
 VALUES (:step, :status, :result)
-""" 
+"""
 
-val update = """
+    val update = """
 UPDATE test_result SET status=:status, result=:result
 WHERE step=:step
-""" 
+"""
 
-val selectStatus = """
+    val selectStatus = """
 SELECT status FROM test_result WHERE step=:step
-""" 
+"""
 
-val selectResult = """
+    val selectResult = """
 SELECT result FROM test_result WHERE step:=step
- """ 
+ """
 }
